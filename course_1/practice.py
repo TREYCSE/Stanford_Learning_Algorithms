08acb9514f090d7bb463f6c29373e9d347d46068
trey_weathers <treychelle.weathers2017@gmail.com>
2023-05-29 16:41:34 -0400

    course 1 week 1

---
+++ course_1/practice.py
@@ -0,0 +1,34 @@
+#how much do I need to install in the terminal?
+import numpy as np
+#%matplotlib widget - what is wrong with the syntax for this, is there anything deep AI specific that I would need to replace to get this to work?
+import matplotlib.pyplot as plt
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+x_train = np.array([1.0, 2.0])           #(size in 1000 square feet)
+y_train = np.array([300.0, 500.0])           #(price in 1000s of dollars)
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum = cost_sum + cost  
+    total_cost = (1 / (2 * m)) * cost_sum  
+
+    return total_cost
+
+plt_intuition(x_train,y_train)
